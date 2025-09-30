e37cc413654048764fb37ddcb02a0307b86e8a94
Pankaj Singh <pankajsingh1work@gmail.com>
2025-09-30 18:39:50 +0530

    deleting next based portfolio filees

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      unoptimized: true, // Allows images from any external source
-    },
-  };
-  
-  export default nextConfig;
-  
